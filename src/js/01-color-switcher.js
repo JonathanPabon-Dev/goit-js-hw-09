e133e8dd98250f83585c1c9c4166063237b2aff7
JonathanPabon-Dev <jonathanpabon.dev@gmail.com>
2024-02-07 20:06:11 -0500

    Ejercicio 1 resuelto

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timer = null;
 
+startBtn.addEventListener('click', () => {
+  timer = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  body.style.removeProperty('background-color');
+  clearInterval(timer);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
