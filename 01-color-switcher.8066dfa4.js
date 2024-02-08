48db8427e7e6c335ddb769d956c36d8660c5c323
JonathanPabon-Dev <134806564+JonathanPabon-Dev@users.noreply.github.com>
2024-02-08 01:06:37 +0000

    Deploying to gh-pages from @ JonathanPabon-Dev/goit-js-hw-09@e133e8dd98250f83585c1c9c4166063237b2aff7 🚀

---
+++ 01-color-switcher.8066dfa4.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]"),n=null;e.addEventListener("click",(function(){n=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),o.addEventListener("click",(function(){t.style.removeProperty("background-color"),clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.8066dfa4.js.map
