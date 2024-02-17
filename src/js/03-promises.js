e83b2fd59f0912b952343a50647a42e85dcf4a38
JonathanPabon-Dev <jonathanpabon.dev@gmail.com>
2024-02-17 14:44:10 -0500

    Tarea 3 terminada

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const delayInput = document.querySelector('input[name="delay"]');
+const stepInput = document.querySelector('input[name="step"]');
+const amountInput = document.querySelector('input[name="amount"]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
     // Reject
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  delay = parseInt(delayInput.value);
+  step = parseInt(stepInput.value);
+  amount = parseInt(amountInput.value);
+
+  if (amount > 0) {
+    let timer = null;
+    setTimeout(() => {
+      let position = 1;
+      createPromise(position, delay);
+      timer = setInterval(() => {
+        delay += step;
+        console.log(position);
+        position++;
+        if (position > amount) {
+          clearInterval(timer);
+        } else {
+          createPromise(position, delay);
+        }
+      }, step);
+    }, delay);
+  }
+});
